7d81133f45b92435061147891c1b9b086a252491
Wojciech Granicki <linkedin.com/in/granicki>
2017-09-18 18:04:24 +0200

    Initial Commit

---
+++ src/main/java/wojciechgranicki/kata19/Kata19Solution.java
@@ -0,0 +1,12 @@
+package wojciechgranicki.kata19;
+
+/**
+ * Created by wojciechgranicki on 18.09.2017.
+ */
+public interface Kata19Solution {
+
+    void loadDictionary(String filePath);
+    String findShortestPath(String begin, String end);
+
+
+}
---
+++ src/test/java/wojciechgranicki/kata19/Kata19SolutionTest.java
@@ -0,0 +1,23 @@
+package wojciechgranicki.kata19;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wojciechgranicki on 18.09.2017.
+ */
+public class Kata19SolutionTest {
+    Kata19Solution kata19Solution;
+
+    @Test
+    public void loadDictionary() throws Exception {
+
+    }
+
+    @Test
+    public void findShortestPath() throws Exception {
+
+    }
+
+}
