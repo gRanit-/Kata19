38b7e2d5bb3d2f9e3ca5ab1a6cc4e5fe8c60b6d6
Wojciech Granicki <linkedin.com/in/granicki>
2017-09-24 17:03:09 +0200

    Implemented a working solution ( need refactoring/code cleaning and proper tests)

--- src/main/java/wojciechgranicki/kata19/Kata19Solution.java
+++ src/main/java/wojciechgranicki/kata19/Kata19Solution.java
@@ -1,12 +1,14 @@
 package wojciechgranicki.kata19;
 
+import java.net.URISyntaxException;
+
 /**
  * Created by wojciechgranicki on 18.09.2017.
  */
 public interface Kata19Solution {
 
-    void loadDictionary(String filePath);
-    String findShortestPath(String begin, String end);
+    void loadDictionary(String filePath) throws URISyntaxException;
+    String findShortestWordChain(String begin, String end);
 
 
 }
